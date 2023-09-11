dab8d31ef8aeb245a382a9ec0ca95c627a9cfe28
Felipe Ignasio Bascunan Soto <fbascuna@c2r6s2.42malaga.com>
2023-09-11 17:01:12 +0200

    second

---
+++ Libft/Libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 16:53:21 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/09/11 16:53:47 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	ft_isalpha(int c);
+
+#endif
