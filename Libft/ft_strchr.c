e0079bfed809904cb002246493c5097c957cdc57
mikema <felipeibstic@gmail.com>
2023-09-18 19:16:53 +0200

    hola

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,10 +6,21 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 19:00:56 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/09/18 19:01:49 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:16:07 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (0);
+}
