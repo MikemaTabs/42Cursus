c1d4c7629ee0ff60d88f40aedb45dfec7aa911f6
mikema <felipeibstic@gmail.com>
2023-10-08 16:35:28 +0200

    ft_putstr_fd completado

---
+++ Libft/ft_putstr_fd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 16:28:39 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/10/08 16:35:16 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+/* 
+int	main(void)
+{
+    char str[] = "hola";
+    ft_putstr_fd(str, 1);
+    return (0);
+}
+ */
