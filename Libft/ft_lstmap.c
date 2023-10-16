4b775d1c989fe33e33b32258a09d6a2b7f4dce30
mikema <felipeibstic@gmail.com>
2023-10-16 15:18:03 +0200

    lstmap perfecto

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,33 +6,40 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:57:01 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/10/10 13:57:33 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:17:28 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	*free_stuff(void **content, t_list **lst, void (*del)(void *))
+{
+	del((*content));
+	ft_lstclear(lst, del);
+	return (NULL);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*nlist;
-	t_list	*ptr;
+	t_list	*original_lst;
+	t_list	*new_lst_item;
+	t_list	*new_list;
+	void	*new_content;
 
-	if (!lst)
+	if (lst == NULL)
 		return (NULL);
-	nlist = 0;
-	while (lst)
-	{
-		ptr = ft_lstnew(f(lst->content));
-		if (ptr)
-		{
-			ft_lstadd_back(&nlist, ptr);
-			lst = lst->next;
-		}
-		else
-		{
-			ft_lstclear(&ptr, del);
-			return (0);
-		}
+	original_lst = lst;
+	new_list = NULL;
+	while (lst != NULL)
+	{	
+		new_content = f(lst->content);
+		if (new_content == NULL)
+			return (free_stuff(&new_content, &new_list, del));
+		new_lst_item = ft_lstnew(new_content);
+		if (new_lst_item == NULL)
+			return (free_stuff(&new_content, &new_list, del));
+		ft_lstadd_back(&new_list, new_lst_item);
+		lst = lst->next;
 	}
-	return (nlist);
+	return (new_list);
 }
