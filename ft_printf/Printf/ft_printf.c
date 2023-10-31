8520fd5ced58eb12495204093fbf76f729dcf53f
mikema <felipeibstic@gmail.com>
2023-10-31 14:11:07 +0100

    makefile perfected. reorganized files.

---
+++ ft_printf/Printf/ft_printf.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 12:54:57 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/10/30 15:56:59 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_printf_check(char s, va_list args, int *len, int *i)
+{
+	if (s == 's')
+	
+}
+
+int	ft_printf(const char *string, ...)
+{
+	
+}
