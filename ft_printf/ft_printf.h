81445006e3f0b69709077707f63d2aa695eb6a8a
mikema <felipeibstic@gmail.com>
2023-11-02 17:25:33 +0100

    Makefile and ft_printf.h updated

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 12:46:42 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/10/30 16:30:53 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:21:52 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # include "Libft/libft.h"
 
 int	ft_printf(const char *string, ...);
-void	ft_string(char *arg, int *len);
+int	ft_arg_perc(void);
+int	ft_arg_c(int c);
+int	ft_arg_s(char *s);
 
 #endif
