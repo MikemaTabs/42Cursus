75cb5ecc3d15a4a3a0f7eeb4c4954f3c0ba8e0f1
mikema <felipeibstic@gmail.com>
2023-11-02 17:26:11 +0100

    added args to ft_printf.c aux function

--- ft_printf/Printf/ft_printf.c
+++ ft_printf/Printf/ft_printf.c
@@ -6,15 +6,23 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 12:54:57 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/10/30 15:56:59 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:20:26 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_printf_check(char s, va_list args, int *len, int *i)
+static int	ft_printf_check(va_list arg, char c)
 {
-	if (s == 's')
+	int	bytes;
+	
+	bytes = 0;
+	if (c == '%')
+		bytes += ft_arg_perc();
+	if (c == 'c')
+		bytes += ft_arg_c(va_arg(arg, int));
+	if (c == 's')
+		bytes += ft_arg_s(va_arg(arg, char *));
 	
 }
 
