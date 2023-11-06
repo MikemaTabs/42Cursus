7f4016b4945a9f992e205fc2b71485bff64733fd
mikema <felipeibstic@gmail.com>
2023-11-06 12:52:22 +0100

    ft_reverse_str created. all aux functions created.

---
+++ ft_printf/Printf/ft_reverse_str.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reverse_str.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 12:39:25 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/11/06 12:42:09 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_reverse_str(char *str)
+{
+	int	i;
+	int	bytes;
+
+	bytes = ft_strlen(str);
+	i = bytes - 1;
+	while (i >= 0)
+		write(1, &str[i--], sizeof(char) * 1);
+	return (bytes);
+}
