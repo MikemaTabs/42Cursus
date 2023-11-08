db4d728e2b48f9f5268dba825169a3c14f2cbc09
mikema <felipeibstic@gmail.com>
2023-11-08 16:13:02 +0100

    ft_arg_x updated

--- ft_printf/printf/ft_arg_x.c
+++ ft_printf/printf/ft_arg_x.c
@@ -6,7 +6,7 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:08:19 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/08 16:09:03 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:12:46 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int	ft_arg_x(unsigned int nbr, char type)
 	i = 0;
 	if (!nbr)
 		return (write(1, "0", sizeof(char) * 1));
-	num = (char *)malloc((ft_hex_length(nbr) + 1) * sizeof(char));
+	num = (char *)malloc((ft_hex_len(nbr) + 1) * sizeof(char));
 	while (nbr)
 	{
-		num[i] = ft_decimal_converter_to_hex(nbr % 16, type);
+		num[i] = ft_decimal_to_hex(nbr % 16, type);
 		nbr /= 16;
 		i++;
 	}
 	num[i] = '\0';
-	bytes = ft_print_reversed_str(num);
+	bytes = ft_reverse_str(num);
 	ft_free_ptr(&num);
 	return (bytes);
 }
