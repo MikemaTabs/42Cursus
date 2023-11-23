454616c8e65c1de6bc5eaf5f603549619385e9b3
mikema <felipeibstic@gmail.com>
2023-11-23 17:31:35 +0100

    proto function started.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,18 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:47:53 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/21 17:10:32 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:10:03 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int	fd)
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buff_line;
+	static char	*backup;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buff_line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	
+}
