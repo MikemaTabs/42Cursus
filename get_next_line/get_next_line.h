402d6915cc5a3f8922ac5d2dd1b9bfdad1108f0b
mikema <felipeibstic@gmail.com>
2023-11-23 17:31:13 +0100

    header file created.

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,21 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:47:56 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/17 16:47:57 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:13:03 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
