f1904a743b0b5142c6f2ea510bf4331e11cd5e0d
mikema <felipeibstic@gmail.com>
2023-11-24 16:17:53 +0100

    utils created. might change later.

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,82 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:47:49 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/17 16:47:50 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:16:26 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str3;
+	size_t	size3;
+
+	if (!s1 || !s2)
+		return (0);
+	size3 = ft_strlen(s1) + ft_strlen(s2);
+	str3 = (char *)malloc (size3 + 1);
+	if (!str3)
+		return (0);
+	ft_memcpy(str3, s1, ft_strlen(s1));
+	ft_memcpy(str3 + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	return (str3);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if ((!*str && (char)c == '\0'))
+		return ((char *)str);
+	while (*str != '\0')
+	{
+		if (*str == (char) c)
+			return ((char *) str);
+		str++;
+		if (*str == (char) c)
+			return ((char *) str);
+	}
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	size;
+
+	size = ft_strlen(s);
+	if (size <= start)
+		return (ft_strdup(""));
+	if (size - start < len)
+		len = size - start;
+	substr = (char *)malloc (len + 1);
+	if (!substr)
+		return (0);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!src && !dest)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)(dest + i)) = *((unsigned char *)(src + i));
+		i++;
+	}
+	return (dest);
+}
