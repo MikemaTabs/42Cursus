2b3636294a2a5a6c034a165941128a59bce81aeb
mikema <felipeibstic@gmail.com>
2023-11-27 18:07:38 +0100

    function name fixed

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:47:56 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/27 18:03:40 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:07:34 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_strchr(const char *str, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strdup(const char *s1);
-char	*ft_cpyfile_to_backup(int fd, char *buff_line, char *backup);
+static char	ft_cpyfile_to_backup(int fd, char *buff_line, char *backup);
 char	*ft_extract_line(char *line);
 
 #endif
