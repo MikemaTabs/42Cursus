2933b571dfb552e5754dc293d0bba0899f795ca6
mikema <felipeibstic@gmail.com>
2023-11-27 18:04:39 +0100

    functions updated.

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:47:56 by fbascuna          #+#    #+#             */
-/*   Updated: 2023/11/24 17:27:12 by fbascuna         ###   ########.fr       */
+/*   Updated: 2023/11/27 18:03:40 by fbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -27,5 +28,8 @@ size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *str, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *s1);
+char	*ft_cpyfile_to_backup(int fd, char *buff_line, char *backup);
+char	*ft_extract_line(char *line);
 
 #endif
