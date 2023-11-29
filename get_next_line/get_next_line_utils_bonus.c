e112b63c5c38deef3409fce752c74c4c24e6ec0c
mikema <felipeibstic@gmail.com>
2023-11-29 14:01:59 +0100

    bonus part updated.

---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 14:00:01 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/11/29 14:00:03 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str3;
+	size_t	size3;
+
+	if (!s1 || !s2)
+		return (0);
+	size3 = ft_strlen(s1) + ft_strlen(s2);
+	str3 = (char *)malloc (size3 + 1);
+	if (!str3)
+		return (0);
+	ft_memcpy(str3, s1, ft_strlen(s1));
+	ft_memcpy(str3 + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	return (str3);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if ((!*str && (char)c == '\0'))
+		return ((char *)str);
+	while (*str != '\0')
+	{
+		if (*str == (char) c)
+			return ((char *) str);
+		str++;
+		if (*str == (char) c)
+			return ((char *) str);
+	}
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	size;
+
+	size = ft_strlen(s);
+	if (size <= start)
+	{
+		substr = (char *)malloc(sizeof(char));
+		if (!substr)
+			return (0);
+		substr[0] = '\0';
+		return (substr);
+	}
+	if (size - start < len)
+		len = size - start;
+	substr = (char *)malloc (len + 1);
+	if (!substr)
+		return (0);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!src && !dest)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)(dest + i)) = *((unsigned char *)(src + i));
+		i++;
+	}
+	return (dest);
+}
