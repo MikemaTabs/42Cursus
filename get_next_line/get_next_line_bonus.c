afafaab3d90aeeadad683aeb927a3519640b3c8c
mikema <felipeibstic@gmail.com>
2023-12-01 18:25:09 +0100

    bonus done.

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbascuna <fbascuna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 18:16:23 by fbascuna          #+#    #+#             */
+/*   Updated: 2023/12/01 18:24:30 by fbascuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*s2;
+
+	len = ft_strlen (s1) + 1;
+	s2 = malloc (len);
+	if (!s2)
+		return (0);
+	ft_memcpy (s2, s1, len);
+	return ((char *) s2);
+}
+
+static char	*ft_cpyfile_to_backup(int fd, char *buf, char *backup)
+{
+	int		read_line;
+	char	*temp;
+
+	read_line = 1;
+	while (read_line != '\0')
+	{
+		read_line = read(fd, buf, BUFFER_SIZE);
+		if (read_line == -1)
+			return (NULL);
+		else if (read_line == 0)
+			break ;
+		buf[read_line] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		temp = backup;
+		backup = ft_strjoin(temp, buf);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr (buf, '\n') && ft_strchr (buf, '\0'))
+			break ;
+		else if (ft_strchr (buf, '\n'))
+			break ;
+	}
+	return (backup);
+}
+
+static char	*ft_extract_bytes_after_newline(char *line)
+{
+	size_t	count;
+	char	*backup;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (count == ft_strlen(line) - 1)
+		return (NULL);
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (backup[0] == '\0')
+	{
+		free(backup);
+		backup = NULL;
+	}
+	line[count + 1] = '\0';
+	return (backup);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*backup[BUFFER_SIZE];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	line = ft_cpyfile_to_backup(fd, buf, backup[fd]);
+	free(buf);
+	if (!line)
+		return (free(backup[fd]), backup[fd] = NULL, NULL);
+	backup[fd] = ft_extract_bytes_after_newline(line);
+	return (line);
+}
